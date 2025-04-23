b0da86154440b220ced8a928dd8d1d153218d10a
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2025-04-24 01:16:55 +0200

    Changement de structure front-end
    Les VBOX deviennent des Pane afin de pouvoir contrôler leur position manuellement(avec les pixels), ajout du logo avec la vue qui va avec.

--- src/main/java/vue/ApplicationUI.java
+++ src/main/java/vue/ApplicationUI.java
@@ -1,9 +1,13 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -25,11 +29,28 @@ public class ApplicationUI extends Application {
 
         // Page d'accueil
 
-
+        Pane thanksTextPane = new Pane();
         Label thanksText = new Label("Merci d’avoir téléchargé");
         thanksText.getStyleClass().add("thanksText");
-        root.setAlignment(javafx.geometry.Pos.TOP_CENTER);
-        root.getChildren().addAll(thanksText);
+        thanksText.setLayoutX(416);
+        thanksText.setLayoutY(78);
+        thanksTextPane.getChildren().addAll(thanksText);
+
+        Pane logoPicPane = new Pane();
+        Image logoPic = new Image("file:css/img/logo.png");
+        ImageView view = new ImageView(logoPic);
+        view.setX(526);
+        view.setY(10);
+        logoPicPane.getChildren().add(view);
+
+        VBox startBox = new VBox();
+        Label startoudouText = new Label("Commençons par créer votre TOuDOuList");
+        startoudouText.getStyleClass().add("startoudouText");
+        startBox.getChildren().addAll(startoudouText);
+        startBox.setAlignment(Pos.TOP_CENTER);
+        root.getChildren().addAll(thanksTextPane);
+        root.getChildren().addAll(logoPicPane);
+        root.getChildren().addAll(startBox);
 
         stage.show();
     }
