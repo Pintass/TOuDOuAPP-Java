f127f09953173486c2d1d5e54533b083b65979b4
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2025-04-24 20:27:31 +0200

    Fin écran de remerciements

--- src/main/java/vue/ApplicationUI.java
+++ src/main/java/vue/ApplicationUI.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,38 +17,56 @@ import java.awt.*;
 import java.io.File;
 
 public class ApplicationUI extends Application {
+    private final static int stageX = 1200;
+    private final static int stageY = 675;
+
     @Override
     public void start(Stage stage) {
 
         // base
         File css = new File("css" + File.separator+"style.css");
         VBox root = new VBox();
-        Scene scene = new Scene(root, 1200, 675);
+        Scene scene = new Scene(root, stageX, stageY);
         scene.getStylesheets().add(css.toURI().toString());
         stage.setScene(scene);
         stage.setTitle("TOuDoU-APP");
+        stage.setResizable(false);
 
         // Page d'accueil
 
         Pane thanksTextPane = new Pane();
         Label thanksText = new Label("Merci d’avoir téléchargé");
         thanksText.getStyleClass().add("thanksText");
-        thanksText.setLayoutX(416);
-        thanksText.setLayoutY(78);
+        thanksText.setLayoutX(415);
+        thanksText.setLayoutY(108);
         thanksTextPane.getChildren().addAll(thanksText);
 
         Pane logoPicPane = new Pane();
         Image logoPic = new Image("file:css/img/logo.png");
         ImageView view = new ImageView(logoPic);
-        view.setX(526);
-        view.setY(10);
+        view.setX(515);
+        view.setY(5);
         logoPicPane.getChildren().add(view);
 
-        VBox startBox = new VBox();
+        Pane startBox = new Pane();
         Label startoudouText = new Label("Commençons par créer votre TOuDOuList");
         startoudouText.getStyleClass().add("startoudouText");
+        startoudouText.setLayoutX(404);
+        startoudouText.setLayoutY(120);
         startBox.getChildren().addAll(startoudouText);
-        startBox.setAlignment(Pos.TOP_CENTER);
+
+        Button nextButton = new Button("Créer une TOuDOuList");
+        nextButton.getStyleClass().add("nextButton");
+        nextButton.setLayoutX(482);
+        nextButton.setLayoutY(170);
+        startBox.getChildren().addAll(nextButton);
+
+        Label creditsText = new Label("Application imaginée et créée par : Pintass. / Daniel");
+        creditsText.getStyleClass().add("creditsText");
+        creditsText.setLayoutX(442);
+        creditsText.setLayoutY(438);
+        startBox.getChildren().addAll(creditsText);
+
         root.getChildren().addAll(thanksTextPane);
         root.getChildren().addAll(logoPicPane);
         root.getChildren().addAll(startBox);
